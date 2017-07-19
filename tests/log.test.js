9dbd362b15ec9189452ce069f8230a7f31177968
Meryl Turner <merylturner08@gmail.com>
2017-07-18 18:17:12 -0700

    added before test to delete all other files

--- tests/log.test.js
+++ tests/log.test.js
@@ -2,21 +2,29 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
-// const app = require('../lib/app');
+const fsExtra = require('fs-extra');
+const path = require('path');
 
 const log = require('../lib/log');
 
 describe('/logs', () => {
     const request = chai.request(log);
 
+    before(() => {
+        fsExtra.emptyDir(path.join(__dirname, '../logs'), err => {
+            if (err) return err;
+            console.log('files have been deleted');
+        });
+    });
+
     it('posts file to logs', done => {
-        request.post('/log')
+        const dataObj = { name: 'meryl', email: 'meryl@meryl.com'};
+        request.post('/logs')
             // .setHeader('Content-Type', 'application/json')
-            .send('hey')
+            .send(dataObj)
             .end((err,res) => {
                 if (err) done(err);
-                assert.equal(res.text,'hey');
-                //response should be {timeStampObj}
+                assert.include(res.text, 'timestamp');
                 done();
             });
     });
